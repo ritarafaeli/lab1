5cba40e5d15f4e0983c0ae9bdefc9f6ecd0cc0bb
Rita Rafaeli <milyazar@gmail.com>
2017-02-02 23:43:02 -0500

    halfway done

--- lab1.ml
+++ lab1.ml
@@ -43,7 +43,7 @@ submit, the Exercise 2 unit test should then pass.
 ......................................................................
 *)
 
-let exercise2 () = failwith "exercise2 not implemented" ;;
+let exercise2 () = 42 ;;
 
 (* Note that your grade on labs is not dependent on your passing the
 unit tests. You'll see that even when a unit test is marked as passed,
@@ -101,7 +101,7 @@ exercise1 below.
 ......................................................................
 *)
 
-let exercise3 () = failwith "exercise3 not implemented" ;;
+let exercise3 () = (-) 5 3 ;;
 
 (* Hint: The OCaml concrete expression
 
@@ -127,30 +127,29 @@ error is generated.
 ......................................................................
 *)
 
-(*   <--- remove this start of comment line
 
-let exercise5a : ??? = 42 ;;
 
-let exercise5b : ??? =
+let exercise5a : int = 42 ;;
+
+let exercise5b : string =
   let greet y = "Hello " ^ y
   in greet "World!";;
 
-let exercise5c : ???  =
+let exercise5c : int * float -> int =
   fun (x, y) -> x + int_of_float y ;;
 
-let exercise5d : ??? =
+let exercise5d : int -> bool =
   fun x -> x < x + 1 ;;
 
-let exercise5e : ??? =
+let exercise5e : bool -> bool list =
   fun x -> if x then [x] else [] ;;
 
-let exercise5f : ??? =
+let exercise5f : int option list =
   [Some 4; Some 2; None; Some 3] ;;
 
-let exercise5g : ??? =
+let exercise5g : ('a option * float option) * bool =
   ((None, Some 42.0), true) ;;
 
-remove this end of comment line too ----> *)
 
 (* Part 3: First-order functional programming
 
@@ -179,10 +178,12 @@ replace the "[]" with the correct functional call.
 ......................................................................
 *)
 
-let square_all (lst : int list) : int list =
-  failwith "square_all not implemented" ;;
+let rec square_all (lst : int list) : int list =
+  match lst with
+	| [] -> []
+	| t::h -> (t * t):: square_all h;;
 
-let exercise6 = [] ;;
+let exercise6 = assert(square_all([2;4])=[4;16]) ;;
 
 (*
 ......................................................................
@@ -191,8 +192,10 @@ list. (What's a sensible return value for the empty list?)
 ......................................................................
 *)
 
-let sum (lst : int list) : int =
-  failwith "sum not implemented" ;;
+let rec sum (lst : int list) : int =
+  match lst with
+  | [] -> 0
+  | h::t -> h + sum t ;; 
   
 (*
 ......................................................................
